4565417f03a5708e031ac8f73af50cfe9bc8e195
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-15 19:23:02 +0530

    Adding script on Tue Nov 15 19:23:02 IST 2016 :octocat:

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
-function createAGrid(arr,url,text) {
-	arr.push("<div class='fl w-100 w-third-ns pa2'>");
-	arr.push("<div class='outline pv4'> <img src='" + url + "' alt=':" + text + ":'/></div>");
-  arr.push("</div>");
-}
-
 function handleData(data) {
+	
 	var emojis = $("#emojis:first");
 	var arr = [];
-	arr.push("<div class='mw9 center ph3-ns'>");
-  arr.push("<div class='cf ph2-ns'>");
-  
-  for(var key in data)
-		createAGrid(arr,data[key],key);
+	var c = 0;
+  for(var key in data) {
+		console.log(arr);
+		if(c % 3 == 0) {
+			arr.push("<div class='mw9 center ph3-ns'>");
+  		arr.push("<div class='cf ph2-ns'>");
+  	}
 
-  arr.push("</div>");
-	arr.push("</div>");	
+		arr.push("<div class='fl w-100 w-third-ns pa2'>");
+		arr.push("<div class='outline pv4'> <img src='" + data[key] + "' alt=':" + key + ":'/></div>");
+	  arr.push("</div>");
+
+  
+		if((c + 1) % 3 == 0) {
+  		arr.push("</div>");
+			arr.push("</div>");	
+  	}
+
+  	c++;
+  }
 	
-	emojis.append(arr);
+	emojis.append(arr.join(""));
+
 }
 
 $(document).ready(function() {
