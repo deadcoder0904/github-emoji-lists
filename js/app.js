e285c32ce1de75c083dcdd73eac56eb6b4771491
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-15 19:28:08 +0530

    Adding script on Tue Nov 15 19:28:08 IST 2016 :octocat:

--- js/app.js
+++ js/app.js
@@ -1,38 +1,30 @@
 function handleData(data) {
-	
 	var emojis = $("#emojis:first");
 	var arr = [];
 	var c = 0;
   for(var key in data) {
-		console.log(arr);
 		if(c % 3 == 0) {
-			arr.push("<div class='mw9 center ph3-ns'>");
+			arr.push("<div class='mw9 center ph3-ns bg-light-green'>");
   		arr.push("<div class='cf ph2-ns'>");
   	}
 
 		arr.push("<div class='fl w-100 w-third-ns pa2'>");
 		arr.push("<div class='outline pv4'> <img src='" + data[key] + "' alt=':" + key + ":'/></div>");
 	  arr.push("</div>");
-
   
 		if((c + 1) % 3 == 0) {
   		arr.push("</div>");
 			arr.push("</div>");	
   	}
-
   	c++;
   }
-	
 	emojis.append(arr.join(""));
-
 }
 
 $(document).ready(function() {
-
 	$.ajax({
 	  	url: "https://api.github.com/emojis"
 		}).done(function(res) {
-	   		console.log(res);
 	   		handleData(res);
 		}).catch(function(err) {
 			$("#emojis").html("<h1 class='b white tc f1'>Request Error !! <i class='em em-frowning'></i>  <br /> Refresh the Webpage !! <i class='em em-smile'></i> </h1>");
