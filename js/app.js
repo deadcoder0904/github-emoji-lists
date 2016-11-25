ec238a9ea029cf3c7cfef7b301d72d265459f583
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-25 16:04:37 +0530

    Added Copy Tooltip

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ function handleData(data) {
   	}
 
 		arr.push("<div class='fl w-100 w-third-ns pa2'>");
-		arr.push("<div class='outline pv4'> <img class='pointer' src='" + data[key] + "' alt=':" + key + ":' /></div>");
+		arr.push("<div class='outline pv4'> <span class='tooltip'><img class='pointer' src='" 
+							+ data[key] + "' data-clipboard-action='copy' data-clipboard-text=':" + key + 
+							":' /><span class='tooltiptext'></span></span></div>");
 	  arr.push("</div>");
   
 		if((c + 1) % 3 == 0) {
@@ -22,11 +24,23 @@ function handleData(data) {
 }
 
 $(document).ready(function() {
+	var clipboard = new Clipboard('.pointer');
+	
 	$.ajax({
 	  	url: "https://api.github.com/emojis"
 		}).done(function(res) {
 	   		handleData(res);
+				$('.pointer').on('mouseover',function() {
+												$('.tooltiptext').text('Copy to Clipboard');
+										}).on('click',function() {
+												$('.tooltiptext').text('Copied');
+										});
 		}).catch(function(err) {
 			$("#emojis").html("<h1 class='b white tc f1'>Request Error !! <i class='em em-frowning'></i>  <br /> Refresh the Webpage !! <i class='em em-smile'></i> </h1>");
 		});
+
+	clipboard.on('success', function(e) {
+	    e.clearSelection();
+	});
+
 });
